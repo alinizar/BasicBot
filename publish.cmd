4505ff97ff887e997ca53130b00572b245179a6f
nizar.ali <nizar.ali@alithya.com>
2018-12-10 15:36:54 -0500

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=mywebappbotnizar-Web-Deploy.pubxml -p:Password=xbAezGzhnG7x6cq7xrhjo5bMZYsdMXN24zbTkQsrRJJncyqWsxu1TqYTmYqx
+
